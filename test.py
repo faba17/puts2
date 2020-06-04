10bc1175bfc1f09e42f1f5e7c1a66f9d702ade6c
番柏力 <faba17@student.bth.se>
2020-06-05 00:27:04 +0200

    subtraction branch added

--- test.py
+++ test.py
@@ -9,21 +9,21 @@ class TestCalculator(
     main.app.testing = True
     self.app = main.app.test_client()
 
-  def test_add(self):
-    solution = self.app.get('/add?a=10&b=12&c=13')
-    self.assertEqual(b'35.000000', solution.data.strip())
+  def test_sub(self):
+    solution = self.app.get('/sub?a=10&b=12&c=13')
+    self.assertEqual(b'-15.000000', solution.data.strip())
 
-    solution = self.app.get('/add?a=2/6&b=3/12&c=1/15')
-    self.assertEqual(b'0.650000', solution.data.strip())
+    solution = self.app.get('/sub?a=2/6&b=3/12&c=1/15')
+    self.assertEqual(b'0.016667', solution.data.strip())
 
-    solution = self.app.get('/add?a=8.9&b=4.2&c=1.3')
-    self.assertEqual(b'14.400000', solution.data.strip())
+    solution = self.app.get('/sub?a=8.9&b=4.2&c=1.3')
+    self.assertEqual(b'3.400000', solution.data.strip())
 
-    solution = self.app.get('/add?a=9&b=3.534&c=1.314')
-    self.assertEqual(b'13.848000', solution.data.strip())
+    solution = self.app.get('/sub?a=9&b=3.534&c=1.314')
+    self.assertEqual(b'4.152000', solution.data.strip())
 
-    solution = self.app.get('/add?a=9/2&b=1&c=2')
-    self.assertEqual(b'7.500000', solution.data.strip()) 
+    solution = self.app.get('/sub?a=9/2&b=1&c=2')
+    self.assertEqual(b'1.500000', solution.data.strip())
 
 if __name__ == '__main__':
   unittest.main()
